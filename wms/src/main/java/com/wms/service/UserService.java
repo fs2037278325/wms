a3f0b9679171f5dfc4a7fdd84000425adc76693e
fashia <2037278325@qq.com>
2024-06-11 13:02:15 +0800

    Initial commit

---
+++ wms/src/main/java/com/wms/service/UserService.java
@@ -0,0 +1,21 @@
+package com.wms.service;
+
+import com.baomidou.mybatisplus.core.conditions.Wrapper;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wms.entity.User;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author wms
+ * @since 2024-06-03
+ */
+public interface UserService extends IService<User> {
+
+    IPage pageC(IPage<User> page);
+
+    IPage pageCC(IPage<User> page, Wrapper wrapper);
+}
